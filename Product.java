8b98ed038f053a6683ebbdc935f25f98db4cf395
Asravin <maisweedfell@.com>
2023-06-26 15:30:34 +0400

    Созданны основные классы проекта

---
+++ Product.java
@@ -0,0 +1,11 @@
+public class Product {
+    public int productID;
+    public int resultContest;
+    public String producText;
+
+    public Product(int productID, int resultContest, String producText){
+        this.productID = productID;
+        this.resultContest = resultContest;
+        this.producText = producText;
+    }
+}
